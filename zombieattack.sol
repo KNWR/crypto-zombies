97f4f51d43913061e1240ce3dee812e1688771c7
Kanwar Sahdra <ksingh7265@gmail.com>
2018-05-02 21:43:37 -0400

    random number

--- zombieattack.sol
+++ zombieattack.sol
@@ -4,5 +4,10 @@ import "./zombiehelper.sol";
 
 contract ZombieBattle is ZombieHelper {
 
+  uint randNonce = 0;
+  // note - potential vulnerability to bad actor nodes here
+  function randMod (uint _modulus) internal returns (uint) {
+    randNonce++;
+    return uint(keccak256(now, msg.sender, randNonce))%_modulus;
+  }
 }
-
