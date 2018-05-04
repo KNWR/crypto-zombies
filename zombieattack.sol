3577505b4cfad2b417d8689f355a5ed3d27b43d2
Kanwar Sahdra <ksingh7265@gmail.com>
2018-05-03 21:27:15 -0400

    zombie atk function

--- zombieattack.sol
+++ zombieattack.sol
@@ -5,9 +5,17 @@ import "./zombiehelper.sol";
 contract ZombieBattle is ZombieHelper {
 
   uint randNonce = 0;
+
+  uint attackVictoryProbability = 70;
+
   // note - potential vulnerability to bad actor nodes here
   function randMod (uint _modulus) internal returns (uint) {
     randNonce++;
     return uint(keccak256(now, msg.sender, randNonce))%_modulus;
   }
+
+  function attack (uint _zombieId, uint _targetId) external {
+    
+  }
+
 }
