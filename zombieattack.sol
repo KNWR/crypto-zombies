59cede31a267e5984036213193283f08b34bb6bf
Kanwar Sahdra <ksingh7265@gmail.com>
2018-05-07 23:54:18 -0400

    defining attack function

--- zombieattack.sol
+++ zombieattack.sol
@@ -3,19 +3,20 @@ pragma solidity ^0.4.19;
 import "./zombiehelper.sol";
 
 contract ZombieBattle is ZombieHelper {
-
   uint randNonce = 0;
-
   uint attackVictoryProbability = 70;
 
   // note - potential vulnerability to bad actor nodes here
-  function randMod (uint _modulus) internal returns (uint) {
+  function randMod(uint _modulus) internal returns(uint) {
     randNonce++;
-    return uint(keccak256(now, msg.sender, randNonce))%_modulus;
+    return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
   }
 
-  function attack (uint _zombieId, uint _targetId) external {
-    
-  }
+  function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    Zombie storage enemyZombie = zombies[_targetId];
+    uint rand = randMod(100);
 
+  }
 }
+
